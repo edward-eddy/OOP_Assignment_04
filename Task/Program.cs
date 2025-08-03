d6a9ff8acf9b2f6142e49a55879084695bef068f
edwar <edwardsameh99@gmail.com>
2025-08-03 22:52:17 +0300

    Assignment + Task

---
+++ Task/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task:
+            //Food food = new Food(5);
+            //food.Show();
+
+            //food++;
+            //food.Show(); 
+            #endregion
+        }
+    }
+}
